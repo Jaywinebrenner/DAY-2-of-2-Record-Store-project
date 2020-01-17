edba8a2c9d33cdd520ffd1a9b31712db2ef2cdfe
Student <example@example.com>
2020-01-16 16:24:15 -0800

    add scope form, scope code in model and ponder adding the scope method in albums controller

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,5 +1,6 @@
 class Album < ApplicationRecord
   scope :rock, -> { where(genre: "Rock") }
+  scope :search, -> (album_parameter) { where("name like ?", "%#{album_parameter}%")}
   has_and_belongs_to_many(:artists)
   has_many :songs, dependent: :destroy
   validates :name, presence: true
